806a1ff5aeb31f3986451f799fbb6f9b5b7c4d1f
Am1d1v <dimadiman1adima@gmail.com>
2024-02-27 15:28:54 +0300

    Navigation Menu Expands

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 
 
+const navigation = document.querySelector('.navigation');
+
+// Toggle class 'active' for navigation bar
+navigation.addEventListener('click', () => {
+    navigation.classList.toggle('active');
+});
